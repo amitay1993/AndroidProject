51b3fba5063faacff5349f3852a122dd5e14e83f
Matan <matan199412@gmail.com>
2020-06-20 20:45:39 +0300

    fixed missiles and music and adding mute button in game

--- app/src/main/java/com/example/androidgameproject/MainActivity.java
+++ app/src/main/java/com/example/androidgameproject/MainActivity.java
@@ -1,32 +1,34 @@
 package com.example.androidgameproject;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mp;
-    boolean sound_bool = true;
+    boolean sound_bool;
+    SharedPreferences soundSP;
+    public static final String USER_SP_FILE ="user_file",SOUND_KEY="sound";
 
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        soundSP=getSharedPreferences(USER_SP_FILE,MODE_PRIVATE);
+        sound_bool=soundSP.getBoolean(SOUND_KEY,true);
 
         fullScreencall();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -39,28 +41,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        final Button sound_btn = findViewById(R.id.sound_btn);
 
         mp = MediaPlayer.create(this,R.raw.opening_sound_liran);
-        mp.start();
         mp.setLooping(true);
+        if(sound_bool)
+            mp.start();
+        else
+            sound_btn.setBackground(getDrawable(R.drawable.ic_mute_icon));
+
 
         ImageView start_anim = findViewById(R.id.start_anim);
         Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.scale_start_anim);
         start_anim.startAnimation(animation1);
 
-        final Button sound_btn = findViewById(R.id.sound_btn);
+
         sound_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (sound_bool) {
-                    sound_btn.setBackground(getDrawable(R.drawable.ic_mute_icon));
-                    sound_bool = false;
-                    mp.pause();
-                } else {
-                    sound_btn.setBackground(getDrawable(R.drawable.ic_speaker_icon));
-                    sound_bool = true;
-                    mp.start();
-                }
+                    @Override
+                    public void onClick(View v) {
+                        if (sound_bool) {
+                            sound_btn.setBackground(getDrawable(R.drawable.ic_mute_icon));
+                            soundSP.edit().putBoolean(SOUND_KEY,false).commit();
+                            sound_bool = false;
+                            mp.pause();
+                        } else {
+                            sound_btn.setBackground(getDrawable(R.drawable.ic_speaker_icon));
+                            soundSP.edit().putBoolean(SOUND_KEY,true).commit();
+                            sound_bool = true;
+                            mp.start();
+                        }
             }
         });
 
@@ -88,13 +97,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        mp.pause();
+        if(sound_bool)
+            mp.pause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mp.start();
+        if(sound_bool)
+            mp.start();
         fullScreencall();
     }
 
