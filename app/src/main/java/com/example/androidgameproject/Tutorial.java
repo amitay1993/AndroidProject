001556cbb24154da80aaf6fbd9f207e840044898
amitay1993 <amitays1993@gmail.com>
2020-06-22 20:09:56 +0300

    tutorial

--- app/src/main/java/com/example/androidgameproject/Tutorial.java
+++ app/src/main/java/com/example/androidgameproject/Tutorial.java
@@ -1,12 +1,15 @@
 package com.example.androidgameproject;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -22,10 +25,17 @@ import androidx.core.content.res.ResourcesCompat;
 public class Tutorial extends AppCompatActivity {
     Point point;
     GameSurfaceTutorial gameSurfaceTutorial;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fullScreencall();
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        fullScreencall();
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         point=new Point();
@@ -40,11 +50,13 @@ public class Tutorial extends AppCompatActivity {
 
         Drawable buttonDrawable =getResources().getDrawable(R.drawable.start_game_tv);
         buttonDrawable.mutate();
+        Drawable buttonDrawa =getResources().getDrawable(R.drawable.start_game_tv);
+        buttonDrawa.mutate();
         Button backBtn = new Button(this);
         Button startGamebtn=new Button(this);
 
         backBtn.setBackground(buttonDrawable);
-        startGamebtn.setBackground(buttonDrawable);
+        startGamebtn.setBackground(buttonDrawa);
 
         LinearLayout.LayoutParams params= new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.setMargins(30,50,30,0);
@@ -54,7 +66,7 @@ public class Tutorial extends AppCompatActivity {
         backBtn.setLayoutParams(params);
         backBtn.setText(R.string.back_to_menu);
         backBtn.setTextColor(Color.WHITE);
-        backBtn.setTextSize(15);
+        backBtn.setTextSize(20);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             Typeface typeface = getResources().getFont(R.font.gendarwo);
@@ -64,21 +76,57 @@ public class Tutorial extends AppCompatActivity {
         backBtn.setTypeface(typeface);
         startGamebtn.setTypeface(typeface);
 
-
-        startGamebtn.setPadding(5,5,5,5);
-        startGamebtn.setTextSize(15);
         startGamebtn.setLayoutParams(params);
+        startGamebtn.setPadding(15,5,15,5);
+
+        startGamebtn.setTextSize(20);
         startGamebtn.setTextColor(Color.WHITE);
         startGamebtn.setText("Start Playing");
 
 
 
         gameWidgets.requestLayout();
-        gameWidgets.addView(backBtn);
         gameWidgets.addView(startGamebtn);
+        gameWidgets.addView(backBtn);
+
         game.addView(gameSurfaceTutorial);
         game.addView(gameWidgets);
         setContentView(game);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Tutorial.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        startGamebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Tutorial.this,GameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+    public void fullScreencall() {
+        if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if(Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent(Tutorial.this,MainActivity.class);
+        finish();
+        startActivity(intent);
 
     }
 }
